a4205891906d95a1975b4a14574fdbd63ae8a15d
Uyanide <pywang0608@foxmail.com>
2025-04-14 09:18:14 +0200

    test: fix charset issues on windows

--- test/compile.ps1
+++ test/compile.ps1
@@ -1,4 +1,32 @@
-. $PSScriptRoot\utils.ps1
+function Log-Info {
+    param ([string]$Message)
+    Write-Host "[INFO] $Message" -ForegroundColor Green
+}
+
+function Log-Error {
+    param ([string]$Message)
+    Write-Host "[ERROR] $Message" -ForegroundColor Red
+}
+
+function Execute-Program {
+    param (
+        [string]$ProgramName,
+        [string[]]$ArgsList
+    )
+
+    try {
+        Log-Info "Executing $ProgramName $ArgsList"
+
+        $process = Start-Process -FilePath $ProgramName -ArgumentList $ArgsList -NoNewWindow -PassThru
+        $process.WaitForExit()
+
+        Log-Info "Program exited with return code: $($process.ExitCode)"
+        return $process.ExitCode
+    } catch {
+        Log-Error "An error occurred while executing the program: $_"
+        return -1
+    }
+}
 
 
 if ($args -contains "--configure") {
--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 
 def log_info(message):
@@ -9,10 +10,26 @@ def log_error(message):
     print(f"[ERROR] {message}")
 
 
+def is_windows():
+    return os.name == 'nt'
+
+
 def execute_program(program_name, args_list):
     try:
-        log_info(f"Executing {program_name} {' '.join(args_list)}")
-        result = subprocess.run([program_name] + args_list, capture_output=True, text=True)
+        if is_windows():
+            encoded_args_list = [arg.encode('mbcs').decode('mbcs') for arg in args_list]
+            encoding = 'mbcs'
+        else:
+            encoded_args_list = args_list
+            encoding = 'utf-8'
+
+        log_info(f"Executing {program_name} {' '.join(encoded_args_list)}")
+        result = subprocess.run(
+            [program_name] + encoded_args_list,
+            capture_output=True,
+            text=True,
+            encoding=encoding
+        )
         log_info(f"Program exited with return code: {result.returncode}")
         if result.stdout:
             log_info(result.stdout)
